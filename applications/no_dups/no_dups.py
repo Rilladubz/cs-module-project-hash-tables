cadd12651ffede2baf8168565eec7e2a079674b9
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2020-08-14 18:08:59 -0400

    finished the no_dups.py file

--- applications/no_dups/no_dups.py
+++ applications/no_dups/no_dups.py
@@ -1,6 +1,23 @@
 def no_dups(s):
     # Your code here
+    word_list = s.split()
 
+    cache = {}
+
+    new_string = ''
+
+    for word in word_list:
+        if word not in cache:
+            cache[word] = word
+            new_string += ''.join(' ' + word)
+
+    split = new_string.split()
+    joined = ' '.join(split)
+
+    if len(word_list) < 2:
+        return s
+    else:
+        return joined
 
 
 if __name__ == "__main__":
--- applications/no_dups/test_no_dups.py
+++ applications/no_dups/test_no_dups.py
@@ -8,12 +8,16 @@ class TestNoDups(unittest.TestCase):
     def test_no_dups(self):
         x = no_dups("")
         self.assertTrue(x == "")
+
         x = no_dups("hello")
         self.assertTrue(x == "hello")
+
         x = no_dups("hello hello")
         self.assertTrue(x == "hello")
+
         x = no_dups("cats dogs fish cats dogs")
         self.assertTrue(x == "cats dogs fish")
+
         x = no_dups("spam spam spam eggs spam sausage spam spam and spam")
         self.assertTrue(x == "spam eggs sausage and")
 
