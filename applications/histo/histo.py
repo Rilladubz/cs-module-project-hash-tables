eb0a3d9657d26e645cd79aa52ea9948f06318184
Samuel Torres <42902403+Rilladubz@users.noreply.github.com>
2020-08-16 02:08:25 -0400

    histo attempt

--- applications/histo/histo.py
+++ applications/histo/histo.py
@@ -1,10 +1,36 @@
 # Your code here
 with open("robin.txt") as f:
     histo = f.read()
-    # print("HISTO: ", histo)
 
     def histo_func(file):
 
-        return file
+        words = {}
 
-# print(histo_func(histo))
+        remove_white_space = " ".join(file.split())
+
+        bad_chars = ['"', ':', '.', '-', '+', '=', '/', '\\',
+                     '|', '[', ']', '{', '}', '(', ')', '*', '^', '&', ',', ';']
+
+        updated_string = ''.join(
+            char.lower() for char in remove_white_space if not char in bad_chars)
+
+        word_list = updated_string
+        word_list = word_list.split()
+
+        for word in word_list:
+            if word not in words:
+                words[word] = 1
+            else:
+                words[word] += 1
+
+        sorted_words = sorted(
+            words.items(), key=lambda x: x[-1], reverse=True)
+        print(sorted_words)
+
+        for key in words:
+            num = words[key]
+            hashtag = "#" * num
+            words[key] = hashtag
+
+
+print(histo_func(histo))
